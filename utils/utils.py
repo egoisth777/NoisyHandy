84e623a093fb6f25b6d2fa0eb6218066862b09cb
Yin Tang <jackeyty@berkeley.edu>
2025-04-15 16:42:15 -0400

    model

---
+++ utils/utils.py
@@ -0,0 +1,34 @@
+import os
+import math
+import torch
+import random
+import numpy as np
+
+def exists(x):
+    return x is not None
+
+def default(val, d):
+    if exists(val):
+        return val
+    return d() if callable(d) else d
+
+def identity(t, *args, **kwargs):
+    return t
+
+def normalize_to_neg_one_to_one(img):
+    return img * 2 - 1
+
+def unnormalize_to_zero_to_one(t):
+    return (t + 1) * 0.5
+
+def seed_everything(seed: int):    
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
